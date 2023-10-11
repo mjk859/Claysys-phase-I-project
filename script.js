369cdc76db0a5f716c32cf978e4751f368971d98
Jaseel <muhammedjaseelk10@gmail.com>
2023-10-11 13:43:02 +0530

    added validate password function and function for calculating password strength

--- script.js
+++ script.js
@@ -8,6 +8,53 @@ function validateEmail() {
     }
 }
 
+function validatePassword() {
+    const passwordInput = document.getElementById('pswd');
+
+    const password = passwordInput.value.trim();
+    const passwordStrength = calculatePasswordStrength(password);
+
+    if (passwordStrength < 2) {
+        alert("Password must be at least moderately strong.");
+        passwordInput.focus();
+    }
+}
+
+function calculatePasswordStrength(password) {
+    // Define your criteria for password strength here.
+    // You can customize these rules based on your requirements.
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChars = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password);
+
+    let strength = 0;
+
+    if (password.length >= minLength) {
+        strength++;
+    }
+
+    if (hasUpperCase) {
+        strength++;
+    }
+
+    if (hasLowerCase) {
+        strength++;
+    }
+
+    if (hasNumbers) {
+        strength++;
+    }
+
+    if (hasSpecialChars) {
+        strength++;
+    }
+
+    return strength;
+}
+
+
 function confirmPassword() {
     const passwordInput = document.getElementById('pswd');
     const cPasswordInput = document.getElementById('confpswd');
