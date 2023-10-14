7c89dd61f8b2ccdeeab5a69f0ad82bd14bd05f48
Jaseel <muhammedjaseelk10@gmail.com>
2023-10-14 09:51:21 +0530

    added future date disabling and many JS functions

--- script.js
+++ script.js
@@ -3,26 +3,62 @@ function validateEmail() {
     const emailInput = document.getElementById('email');
     const email = emailInput.value.trim();
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const emailError = document.getElementById("emailError");
     if (!emailPattern.test(email)) {
-        alert("Please enter a valid email address.")
+        emailError.textContent = "Please enter a valid email id.";
+        return false;
+    } else {
+        emailError.textContent = "";
+        return true;
     }
 }
 
-function validatePassword() {
-    const passwordInput = document.getElementById('pswd');
+function validateUsername() {
+    const usernameInput = document.getElementById('username');
+    const username = usernameInput.value.trim();
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const userNameError = document.getElementById("userNameError");
+    if (!emailPattern.test(username)) {
+        userNameError.textContent = "Please enter a valid email id.";
+        return false;
+    } else {
+        userNameError.textContent = "";
+        return true;
+    }
+}
 
+function validateLPassword() {
+    const passwordInput = document.getElementById('password');
+    const passwordError = document.getElementById("passwordError");
     const password = passwordInput.value.trim();
     const passwordStrength = calculatePasswordStrength(password);
 
     if (passwordStrength < 2) {
-        alert("Password must be at least moderately strong.");
-        passwordInput.focus();
+        passwordError.textContent = "Incorrect password.";
+        return false;
+    } else {
+        passwordError.textContent = "";
+        return true;
     }
 }
 
+function validatePassword() {
+    const passwordInput = document.getElementById('password');
+    const passwordError = document.getElementById("passwordError");
+    const password = passwordInput.value.trim();
+    const passwordStrength = calculatePasswordStrength(password);
+
+    if (passwordStrength < 2) {
+        passwordError.textContent = "Password must be moderately strong.";
+        return false;
+    } else {
+        passwordError.textContent = "";
+        return true;
+    }
+    
+}
+
 function calculatePasswordStrength(password) {
-    // Define your criteria for password strength here.
-    // You can customize these rules based on your requirements.
     const minLength = 8;
     const hasUpperCase = /[A-Z]/.test(password);
     const hasLowerCase = /[a-z]/.test(password);
@@ -56,28 +92,71 @@ function calculatePasswordStrength(password) {
 
 
 function confirmPassword() {
-    const passwordInput = document.getElementById('pswd');
-    const cPasswordInput = document.getElementById('confpswd');
-    const passwordError = document.getElementById("passwordError");
+    const passwordInput = document.getElementById('password');
+    const cPasswordInput = document.getElementById('confirmpassword');
+    const cpasswordError = document.getElementById("cpasswordError");
     const inputPassword = passwordInput.value;
     const inputConfirmPassword = cPasswordInput.value;
     if (inputPassword !== inputConfirmPassword) {
-        passwordError.textContent = "Passwords do not match.";
+        cpasswordError.textContent = "Passwords do not match.";
         return false;
     } else {
-        passwordError.textContent = "";
+        cpasswordError.textContent = "";
         return true;
     }
 }
 
 function validateName() {
+    const nameInput = document.getElementById("name")
+    const nameError = document.getElementById("nameError");
     if (nameInput.value.trim() === '') {
-        alert('Please enter your name.');
+       nameError.textContent = "Please enter your name.";
+        return false;
+    } else {
+        nameError.textContent = "";
+        return true;
     }
 }
 
 function validateMessage() {
+    const messageInput = document.getElementById('message');
+    const messageError = document.getElementById("messageError");
+
     if (messageInput.value.trim() === '') {
-        alert('Please enter a message.');
+        messageError.textContent = "Please enter a message and subject.";
+        return false;
+    } else {
+        messageError.textContent = "";
+        return true;
+    }
+}
+
+function validateFirstName() {
+    const firstNameInput = document.getElementById('firstName');
+    const firstName = firstNameInput.value.trim();
+    const firstNameError = document.getElementById('firstNameError');
+    const namePattern = /^[A-Za-z]+$/;
+
+    if (!namePattern.test(firstName)) {
+        firstNameError.textContent = "Name should contain only letters.";
+        return false;
+    } else {
+        firstNameError.textContent = "";
+        return true;
+    }
+}
+
+function validateLastName() {
+    const lastNameInput = document.getElementById('lastName');
+    const lastName = lastNameInput.value.trim();
+    const lastNameError = document.getElementById('lastNameError');
+    const namePattern = /^[A-Za-z]+$/;
+
+    if (!namePattern.test(lastName)) {
+        lastNameError.textContent = "Name should contain only letters.";
+        return false;
+    } else {
+        lastNameError.textContent = "";
+        return true;
     }
 }
